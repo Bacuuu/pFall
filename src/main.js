3195304f174579580e4b1757210526358ddc3cb1
Bacuuu <823727097@qq.com>
2021-03-03 23:21:55 +0800

    init:增加mock和axios用于数据模拟和请求

--- src/main.js
+++ src/main.js
@@ -2,10 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import vueLazyload from 'vue-lazyload'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(vueLazyload)
+Vue.prototype.$axios = axios;
+
+if(process.env.NODE_ENV === 'development') {
+  const Mock = require('mockjs')
+  Mock.mock('/api/getImg', 'get', () => {
+    const list = []
+    for(let i = 0; i < 5; i++) {
+      const width = Mock.Random.integer(200,800)
+      const height = Mock.Random.integer(100,900)
+      const imgSrc = Mock.Random.image(`${width}x${height}`)
+      const poem = Mock.Random.csentence(10,20);
+      const time = Mock.Random.datetime()
+      list.push({
+        width,
+        height,
+        imgSrc,
+        poem,
+        time
+      })
+    }
+    return list
+  })
+}
 
 new Vue({
   router,
