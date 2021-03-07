e430662a3240e8c35c8faac21d4eabdfb326f65a
Bacuuu <823727097@qq.com>
2021-03-07 22:24:45 +0800

    fix:修改懒加载显示内容

--- src/main.js
+++ src/main.js
@@ -3,17 +3,21 @@ import App from './App.vue'
 import vueLazyload from 'vue-lazyload'
 import router from './router'
 import axios from 'axios'
+const loadingImg = require('./assets/loading.gif')
 
 Vue.config.productionTip = false
 
-Vue.use(vueLazyload)
+Vue.use(vueLazyload, {
+  loading: loadingImg,
+  dispatchEvent: true
+})
 Vue.prototype.$axios = axios;
 
 if(process.env.NODE_ENV === 'development') {
   const Mock = require('mockjs')
   Mock.mock('/api/getImg', 'get', () => {
     const list = []
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < 20; i++) {
       const width = Mock.Random.integer(200,800)
       const height = Mock.Random.integer(100,900)
       const imgSrc = Mock.Random.image(`${width}x${height}`)
