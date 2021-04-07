f10680a013a556d2c8cef14b59fadf89d542c8bc
Bacuuu <823727097@qq.com>
2021-04-07 23:08:50 +0800

    进行后端调试 配置更改

--- src/main.js
+++ src/main.js
@@ -13,27 +13,27 @@ Vue.use(vueLazyload, {
 })
 Vue.prototype.$axios = axios;
 
-if(process.env.NODE_ENV === 'development') {
-  const Mock = require('mockjs')
-  Mock.mock('/api/getImg', 'get', () => {
-    const list = []
-    for(let i = 0; i < 20; i++) {
-      const width = Mock.Random.integer(200,800)
-      const height = Mock.Random.integer(100,900)
-      const imgSrc = Mock.Random.image(`${width}x${height}`)
-      const poem = Mock.Random.csentence(10,20);
-      const time = Mock.Random.datetime()
-      list.push({
-        width,
-        height,
-        imgSrc,
-        poem,
-        time
-      })
-    }
-    return list
-  })
-}
+// if(process.env.NODE_ENV === 'development') {
+//   const Mock = require('mockjs')
+//   Mock.mock('/api/v1/photo/get', 'get', () => {
+//     const list = []
+//     for(let i = 0; i < 20; i++) {
+//       const width = Mock.Random.integer(200,800)
+//       const height = Mock.Random.integer(100,900)
+//       const imgSrc = Mock.Random.image(`${width}x${height}`)
+//       const poem = Mock.Random.csentence(10,20);
+//       const time = Mock.Random.datetime()
+//       list.push({
+//         width,
+//         height,
+//         imgSrc,
+//         poem,
+//         time
+//       })
+//     }
+//     return list
+//   })
+// }
 
 new Vue({
   router,
